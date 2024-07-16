e0a2c5128af15d5d53654d929fb0d3beb3798c7c
Ankit Jain <ankitjain0296@gmail.com>
2024-07-16 23:52:24 +0530

    migrating repo

---
+++ metadata/deploy.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'provide unable involve when desk kitten expire clean divide other monster ball',
+  'http://localhost:7545'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: '2000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
